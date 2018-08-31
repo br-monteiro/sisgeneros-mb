994e3730b068bc6096b95279c9e90c06f2f723b3
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-08-30 21:31:13 -0300

    Adding configurations files

---
+++ App/Config/DatabaseConfig.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * HTR FIREBIRD FRAMEWORK 2.2 - Copyright (C) <2015>  <BRUNO MONTEIRO>
+ * Framework PHP e MVC para agilizar o desenvolvimento de Aplicativos Web
+ * 
+ * @author Edson Monteiro <bruno.monteirodg@gmail.com>
+ * @file DatabaseConfig.php
+ * @version 0.1
+ * - Class que configura as diretrizes para conexão com o Banco de Dados
+ */
+namespace App\Config;
+
+class DatabaseConfig
+{
+
+    public $db = [
+        // Critografia base64_encode;
+        'servidor' => 'bG9jYWxob3N0', // localhost
+        'banco' => 'dGVzdGU=', // teste
+        'usuario' => 'd2ViYXBw', // root
+        'senha' => 'MTAwNTI4MzY=',
+        'opcoes' => [\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"],
+        // Altere este campo apenas se for usar a Base de Dados Sqlite
+        'sqlite' => 'sisgeneros.db'
+    ];
+
+}
