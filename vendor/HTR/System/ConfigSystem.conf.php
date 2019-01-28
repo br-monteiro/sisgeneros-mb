fe6b497de73cb4e0cac3997ed7ef5db919422a15
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-01-28 20:00:50 -0200

    Move constants to system core

--- vendor/HTR/System/ConfigSystem.conf.php
+++ vendor/HTR/System/ConfigSystem.conf.php
@@ -12,6 +12,20 @@
  * AS CONTANTES DEFINIDAS AQUI NÃO DEVEM SER RENOMEADAS
  */
 
+// URI principal de acesso a aplicação : /
+define('APPDIR', '/app/sisgeneros/');
+// Nome da Aplicação
+define('APPNAM', 'SisGêneros');
+// Versão da Aplicação
+define('APPVER', '1.7');
+// Entidade Padrão usada para login
+define('TBLOGI', 'users');
+// Rota padrão para o formulário de login
+define('CTRLOG', 'acesso/login');
+// Rota padrão para o formulário de troca de senha no primeiro acesso
+define('CTRMOP', 'acesso/mudarsenha');
+// Coluna padrão que indica a necessidade de troca de senha
+define('COLMOP', 'trocar_senha');
 // Diretório padrão onde serão salvos os arquivos de Banco de Dados
 define('DATADR', DRINST . 'App/Database/DbRepository/');
 // Diretório padrão onde serão salvos os arquivos de Backup do Banco de Dados
