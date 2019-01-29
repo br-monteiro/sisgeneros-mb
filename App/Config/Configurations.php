c566150fc93cf9be67a6b571b4a0a358178f93e6
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-01-28 23:40:11 -0200

    Added new configurations class

---
+++ App/Config/Configurations.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Config;
+
+use HTR\System\InternalConfigurations;
+
+class Configurations extends InternalConfigurations
+{
+
+    const DS = DIRECTORY_SEPARATOR;
+    const STR_SALT = '03f3b903d39fa68ede0bbcb2204057fe44a6d49';
+    const DOMAIN = 'www.ceimbe.mb';
+    const ADMIN_CONTACT = 'E-mail: bruno.monteirodg@gmail.com';
+    const PATH_CORE = '/home/edsonmonteiro/workspace/projects/sisgeneros-mb/';
+    const DIR_DATABASE = self::PATH_CORE . 'App/Database/DbRepository/';
+
+    /**
+     * Returns the configurations of htr.json files
+     * @return \stdClass
+     */
+    public static function htrFileConfigs(): \stdClass
+    {
+        $projectDirectory = str_replace('App/Config', '', __DIR__);
+        $file = $projectDirectory . self::DS . 'htr.json';
+
+        if (file_exists($file)) {
+            $fileContent = file_get_contents($file);
+            $object = json_decode($fileContent);
+            if (is_object($object)) {
+                return $object;
+            }
+        }
+
+        return new \stdClass();
+    }
+}
