f60f9ac69a64f6407d8d52111f8a65df50dade05
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-02-02 11:19:00 -0200

    Adjustments on dump database

--- dump.sql
+++ dump.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.1.1 on sáb fev 2 05:37:54 2019
+-- File generated with SQLiteStudio v3.1.1 on sáb fev 2 11:00:23 2019
 --
 -- Text encoding used: UTF-8
 --
@@ -22,7 +22,7 @@ CREATE TABLE avaliacao_fornecedor (
 DROP TABLE IF EXISTS fornecedor;
 
 CREATE TABLE fornecedor (
-    id    INTEGER      PRIMARY KEY AUTOINCREMENT,
+    id    INTEGER      PRIMARY KEY,
     nome  TEXT,
     cnpj  VARCHAR (18),
     dados TEXT
@@ -33,7 +33,7 @@ CREATE TABLE fornecedor (
 DROP TABLE IF EXISTS licitacao;
 
 CREATE TABLE licitacao (
-    id        INTEGER      PRIMARY KEY AUTOINCREMENT,
+    id        INTEGER      PRIMARY KEY,
     numero    VARCHAR (10),
     uasg      INT (6),
     nome_uasg VARCHAR (50),
@@ -47,7 +47,7 @@ CREATE TABLE licitacao (
 DROP TABLE IF EXISTS licitacao_item;
 
 CREATE TABLE licitacao_item (
-    id            INTEGER     PRIMARY KEY AUTOINCREMENT,
+    id            INTEGER     PRIMARY KEY,
     id_lista      INT,
     id_fornecedor INT,
     numero        INT,
@@ -77,27 +77,80 @@ CREATE TABLE om (
     fiel_municiamento_posto   VARCHAR (40) 
 );
 
-INSERT INTO om (id, nome, uasg, indicativo_naval, created_at, updated_at, agente_fiscal, agente_fiscal_posto, gestor_municiamento, gestor_municiamento_posto, fiel_municiamento, fiel_municiamento_posto) VALUES (1, 'OM PADRAO', 123456, 'OMPADR', 1549092471, 1549092471, 'ANGENTE', 'AGENTE POSTO', 'GESTOR', 'GESTOR POSTO', 'FIEL', 'FIEL POSTO');
+INSERT INTO om (
+                   id,
+                   nome,
+                   uasg,
+                   indicativo_naval,
+                   created_at,
+                   updated_at,
+                   agente_fiscal,
+                   agente_fiscal_posto,
+                   gestor_municiamento,
+                   gestor_municiamento_posto,
+                   fiel_municiamento,
+                   fiel_municiamento_posto
+               )
+               VALUES (
+                   1,
+                   'CENTRO DE INTENDÊNCIA DA MARINHA EM BELÉM',
+                   784810,
+                   'CITBEL',
+                   1435688867,
+                   1470966887,
+                   'EDSON BRUNO SOARES MONTEIRO',
+                   'Marinheiro',
+                   'USUARIO',
+                   'Primeiro Tenente',
+                   'USUARIO',
+                   'Soboficial'
+               );
+
+
+-- Table: quadro_avisos
+DROP TABLE IF EXISTS quadro_avisos;
+
+CREATE TABLE quadro_avisos (
+    id              INTEGER       PRIMARY KEY AUTOINCREMENT,
+    titulo          VARCHAR (100) DEFAULT (''),
+    corpo           VARCHAR (256) DEFAULT (''),
+    usuario_criador INT           REFERENCES users (id) ON DELETE CASCADE,
+    created_at      DATE,
+    data_inicio     DATE,
+    data_fim        DATE
+);
+
+
+-- Table: quadro_avisos_lista_oms
+DROP TABLE IF EXISTS quadro_avisos_lista_oms;
+
+CREATE TABLE quadro_avisos_lista_oms (
+    id               INTEGER PRIMARY KEY AUTOINCREMENT,
+    om_id            INTEGER REFERENCES om (id) ON DELETE CASCADE,
+    quadro_avisos_id INTEGER REFERENCES quadro_avisos (id) ON DELETE CASCADE
+);
+
 
 -- Table: solicitacao
 DROP TABLE IF EXISTS solicitacao;
 
 CREATE TABLE solicitacao (
-    id                 INTEGER AUTOINCREMENT,
-    id_licitacao       INT,
-    id_lista           INT,
-    om_id              INTEGER (10),
-    ano                INTEGER (4),
-    numero             INTEGER,
-    status             TEXT,
-    created_at         TIMESTAMP,
-    updated_at         TIMESTAMP,
-    fornecedor_id      INT,
-    nao_licitado       INT (1)      NOT NULL
-                                    DEFAULT (0),
-    numero_nota_fiscal VARCHAR (20),
-    data_entrega       VARCHAR (10),
-    observacao         TEXT,
+    id                   INTEGER,
+    id_licitacao         INT,
+    id_lista             INT,
+    om_id                INTEGER (10),
+    ano                  INTEGER (4),
+    numero               INTEGER,
+    status               TEXT,
+    created_at           TIMESTAMP,
+    updated_at           TIMESTAMP,
+    fornecedor_id        INT,
+    nao_licitado         INT (1)      NOT NULL
+                                      DEFAULT (0),
+    numero_nota_fiscal   VARCHAR (20),
+    data_entrega         VARCHAR (10),
+    observacao           TEXT,
+    lista_desmembramento TEXT         DEFAULT (''),
     PRIMARY KEY (
         id
     ),
@@ -112,7 +165,7 @@ CREATE TABLE solicitacao (
 DROP TABLE IF EXISTS solicitacao_item;
 
 CREATE TABLE solicitacao_item (
-    id                       INTEGER      PRIMARY KEY AUTOINCREMENT,
+    id                       INTEGER      PRIMARY KEY,
     id_lista                 INT,
     item_numero              INT,
     item_nome                VARCHAR (50),
@@ -142,7 +195,37 @@ CREATE TABLE users (
     active       INT (1) 
 );
 
-INSERT INTO users (id, username, password, om_id, name, email, nivel, trocar_senha, last_ip, last_access, created_at, updated_at, active) VALUES (1, 'administrador', '$2y$11$SAEHIb9ulossGd0kQelZhe664b5nGcHdHNW5GhgEXtCkq03j3KwyG', '1', 'Edson Monteiro', 'bruno.monteirodg@gmail.com', 'ADMINISTRADOR', 1, '0', 0, 1549092471, 1549092471, 1);
+INSERT INTO users (
+                      id,
+                      username,
+                      password,
+                      om_id,
+                      name,
+                      email,
+                      nivel,
+                      trocar_senha,
+                      last_ip,
+                      last_access,
+                      created_at,
+                      updated_at,
+                      active
+                  )
+                  VALUES (
+                      1,
+                      'administrador',
+                      '$2y$11$UPoZuzoDYklTJG5QiCcAsOL9.tN4Z0ioDcK1JUJWZ3A7EvJK49cWG',
+                      '1',
+                      'Administrador',
+                      'bruno.monteirodg@gmail.com',
+                      'ADMINISTRADOR',
+                      0,
+                      '1',
+                      1,
+                      1549092471,
+                      1549111926,
+                      1
+                  );
+
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
