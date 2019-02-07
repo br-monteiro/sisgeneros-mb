de60baa7c096d76d23ecb5f70b2c9ab5b121c2c9
br-monteiro <edson.monteiro@chaordicsystems.com>
2019-02-07 00:44:06 -0200

    added suffix into limitString(string, [[int], [string]]): string method

--- App/Helpers/View.php
+++ App/Helpers/View.php
@@ -8,12 +8,13 @@ class View
      * Slice the string passed by parameter according the length informed
      * @param string $str The string
      * @param int $length The length used to slice
+     * @param string $suffix The suffix concatened on sliced string
      * @return string The sliced string
      */
-    public static function limitString(string $str, int $length = 30): string
+    public static function limitString(string $str, int $length = 30, string $suffix = '...'): string
     {
         if (strlen($str) > $length) {
-            return substr($str, 0, $length);
+            return substr($str, 0, $length) . $suffix;
         }
         return $str;
     }
