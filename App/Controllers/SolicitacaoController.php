289a464911ad6479797e92df13616734439de123
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-16 16:19:46 -0300

    Added status check, to forward to request detail

--- App/Controllers/SolicitacaoController.php
+++ App/Controllers/SolicitacaoController.php
@@ -281,6 +281,13 @@ class SolicitacaoController extends Controller implements CtrlInterface
         (new SolicitacaoModel())->processStatus($id, $status, $action);
 
         header('location: ' . $this->view->controller);
+
+        if($status == 'APROVADO' && $action == 'PROXIMO') {
+            $solicitacao = (new SolicitacaoModel())->findById($id);
+            header('location: '
+                . $this->view->controller
+                . 'detalhar/idlista/'.$solicitacao['id_lista']);
+        }
     }
 
     public function processarnaolicitadoAction()
