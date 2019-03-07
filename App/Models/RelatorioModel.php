a6065c2cbe66b25837f293820b941699977f52d1
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-03-07 13:56:03 -0300

    Adjustments in files from 'report'

--- App/Models/RelatorioModel.php
+++ App/Models/RelatorioModel.php
@@ -19,29 +19,30 @@ class RelatorioModel extends CRUD
     public function paginatorDeliveryReport(ControllerAbstract $controller): self
     {
         $select = ""
-            . " ev.*, supp.name AS suppliers_name, "
-            . " lic.number AS biddings_number, "
-            . " lic.uasg_name AS biddings_uasg_name, "
-            . " sol.number AS requests_number, "
-            . " oms.naval_indicative AS om_naval_indicative, "
-            . " sol.created_at,"
-            . " sol.status AS requests_status ";
+            . " ev.evaluation, 
+                supp.name AS suppliers_name,
+                lic.number AS biddings_number,
+                lic.uasg_name AS biddings_uasg_name,
+                req.number AS requests_number,
+                oms.naval_indicative AS om_naval_indicative,
+                req.created_at,
+                req.status AS requests_status ";
         $innerJoin = ""
             . " AS ev "
+            . " INNER JOIN requests AS req "
+            . "     ON req.id = ev.requests_id "
             . " INNER JOIN suppliers AS supp "
-            . "     ON supp.id = ev.suppliers_id "
+            . "     ON supp.id = req.suppliers_id "
             . " INNER JOIN biddings AS lic "
-            . "     ON lic.id = ev.biddings_id "
-            . " INNER JOIN requests AS sol "
-            . "     ON sol.id = ev.requests_id "
+            . "     ON lic.id = req.biddings_id "
             . " INNER JOIN oms "
-            . "     ON oms.id = sol.oms_id ";
+            . "     ON oms.id = req.oms_id ";
         $dados = [
             'entidade' => $this->entidade . $innerJoin,
             'select' => $select,
             'pagina' => $controller->getParametro('pagina'),
             'maxResult' => 500,
-            'orderBy' => 'sol.created_at ASC',
+            'orderBy' => 'req.created_at ASC',
             'bindValue' => []
         ];
 
@@ -56,9 +57,9 @@ class RelatorioModel extends CRUD
         // search by Fornecedor
         if (isset($params['suppliers']) && intval($params['suppliers']) !== 0) {
             if (isset($dados['where'])) {
-                $dados['where'] .= ' AND ev.suppliers_id = :suppId ';
+                $dados['where'] .= ' AND req.suppliers_id = :suppId ';
             } else {
-                $dados['where'] = ' ev.suppliers_id = :suppId ';
+                $dados['where'] = ' req.suppliers_id = :suppId ';
             }
             $dados['bindValue'][':suppId'] = $params['suppliers'];
         }
@@ -68,9 +69,9 @@ class RelatorioModel extends CRUD
             $date = Utils::dateDatabaseFormate($params['dateInit']);
 
             if (isset($dados['where'])) {
-                $dados['where'] .= ' AND sol.created_at >= :dateInit ';
+                $dados['where'] .= ' AND req.created_at >= :dateInit ';
             } else {
-                $dados['where'] = ' sol.created_at >= :dateInit ';
+                $dados['where'] = ' req.created_at >= :dateInit ';
             }
             $dados['bindValue'][':dateInit'] = $date;
         }
@@ -80,9 +81,9 @@ class RelatorioModel extends CRUD
             $date = Utils::dateDatabaseFormate($params['dateEnd']);
 
             if (isset($dados['where'])) {
-                $dados['where'] .= ' AND sol.created_at <= :dateEnd ';
+                $dados['where'] .= ' AND req.created_at <= :dateEnd ';
             } else {
-                $dados['where'] = ' sol.created_at <= :dateEnd ';
+                $dados['where'] = ' req.created_at <= :dateEnd ';
             }
             $dados['bindValue'][':dateEnd'] = $date;
         }
